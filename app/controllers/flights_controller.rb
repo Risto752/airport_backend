02cf42025b532b52a399c9a254ecc36d0ee73445
Risto Maric <ogi.vgd942@gmail.com>
2023-10-05 15:06:24 +0200

    CRUD

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,17 +3,21 @@ class FlightsController < ApplicationController
     query = Flight.all
 
     permitted_params = params.permit(:company_id, :stops, :from, :to, :select_sort_state, :sort_toggle,
-      :business_min, :business_max, :economic_min, :economic_max)
+                                     :business_min, :business_max, :economic_min, :economic_max)
 
     if permitted_params.present?
       query = query.where(company_id: permitted_params[:company_id]) if permitted_params[:company_id].present?
       query = query.where(stops: permitted_params[:stops]) if permitted_params[:stops].present?
 
-      query = query.joins("LEFT JOIN airports AS from_airport ON flights.from = from_airport.id")
-            .where('from_airport.name LIKE ?', "%#{permitted_params[:from]}%") if permitted_params[:from].present?
+      if permitted_params[:from].present?
+        query = query.joins('LEFT JOIN airports AS from_airport ON flights.from = from_airport.id')
+                     .where('from_airport.name LIKE ?', "%#{permitted_params[:from]}%")
+      end
 
-      query = query.joins("LEFT JOIN airports AS to_airport ON flights.to = to_airport.id")
-            .where('to_airport.name LIKE ?', "%#{permitted_params[:to]}%") if permitted_params[:to].present?
+      if permitted_params[:to].present?
+        query = query.joins('LEFT JOIN airports AS to_airport ON flights.to = to_airport.id')
+                     .where('to_airport.name LIKE ?', "%#{permitted_params[:to]}%")
+      end
 
       if permitted_params[:select_sort_state].present? && permitted_params[:sort_toggle].present?
         column_name = case permitted_params[:select_sort_state]
@@ -23,9 +27,7 @@ class FlightsController < ApplicationController
                         'economic_price'
                       when 'fastest'
                         'flight_duration'
-                      else
-                        nil
-                    end
+                      end
 
         if column_name
           direction = permitted_params[:sort_toggle] == 'asc' ? 'asc' : 'desc'
@@ -33,19 +35,91 @@ class FlightsController < ApplicationController
         end
       end
 
-      query = query.where('flights.business_price >= ? AND flights.business_price <= ?', 
+      query = query.where('flights.business_price >= ? AND flights.business_price <= ?',
                           permitted_params[:business_min], permitted_params[:business_max])
 
-      query = query.where('flights.economic_price >= ? AND flights.economic_price <= ?', 
+      query = query.where('flights.economic_price >= ? AND flights.economic_price <= ?',
                           permitted_params[:economic_min], permitted_params[:economic_max])
 
       @flights = query.all
       render json: @flights, each_serializer: FlightSerializer
 
-      
     else
       @flights = Flight.all
-    render json: @flights, each_serializer: FlightSerializer
+      render json: @flights, each_serializer: FlightSerializer
     end
   end
+
+  def create
+    flight_params = params.permit(
+      flight: %i[
+        from
+        to
+        departure_date
+        business_price
+        economic_price
+        flight_duration
+        stops
+        company_id
+      ]
+    )
+
+    @flight = Flight.new(flight_params[:flight])
+
+    if @flight.save
+      render json: @flight, status: :created
+    else
+      render json: @flight.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+
+    @flight = Flight.find(params[:id]) 
+
+    if @flight.destroy
+      flash[:notice] = 'Flight deleted successfully'
+    else
+      flash[:alert] = 'Failed to delete flight'
+    end
+    
+  end
+
+
+  def show 
+
+    @flight = Flight.find(params[:id])
+    render json: @flight
+
+  end
+
+
+  def update
+
+    flight = Flight.find(params[:id])
+    if flight.update(flight_params)
+      render json: flight, status: :ok
+    else
+      render json: { errors: flight.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(
+      :from,
+      :to,
+      :departure_date,
+      :business_price,
+      :economic_price,
+      :flight_duration,
+      :stops,
+      :company_id
+    )
+
+  end
+
+
+
 end
--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -1,5 +1,5 @@
 class FlightSerializer < ActiveModel::Serializer
-  attributes :id, :from, :to, :departure_date, :business_price, :economic_price, :flight_duration, :stops
+  attributes :id, :from, :to, :departure_date, :business_price, :economic_price, :flight_duration, :stops,:from_id, :to_id, :company_id
 
   def from
     Airport.find(object.from).name
@@ -8,4 +8,17 @@ class FlightSerializer < ActiveModel::Serializer
   def to
     Airport.find(object.to).name
   end
+
+  def from_id
+    object.from
+  end
+
+  def to_id
+    object.to
+  end
+
+  def company_id
+    object.company_id
+  end
+
 end
